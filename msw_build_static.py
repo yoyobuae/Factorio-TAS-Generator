a18521a5133e2a8a8831efedf191b26cdb3fa9e2
yoyobuae <yoyobuae@users.noreply.github.com>
2026-05-23 12:29:07 -0500

    Update msw_build_static.py

--- msw_build_static.py
+++ msw_build_static.py
@@ -1,3 +1,4 @@
+
 # A script to change *all* project files within the WxWidgets codebase, to build them
 # for static run-time. This seems to be the only way to do it with more
 # recent versions of Visual Studio.
@@ -16,11 +17,12 @@ if len(sys.argv) < 2:
     
 
 P = pathlib.Path(sys.argv[1]).resolve()
-for F in P.glob("*.props"):
+for F in P.glob("*.vcxproj"):
     if F.is_file():
         print("msw_build_static.py is processing file: {0}".format(str(F)))
         T = F.read_text()
-        T = re.sub(r'<wxRuntimeLibs>dynamic</wxRuntimeLibs>', '<wxRuntimeLibs>static</wxRuntimeLibs>', T)
+        T = re.sub(r'\bMultiThreadedDLL\b', 'MultiThreaded', T)              # Replaces whole word only
+        T = re.sub(r'\bMultiThreadedDebugDLL\b', 'MultiThreadedDebug', T)    # Replaces whole word only
         F.write_text(T)
 
 sys.exit(0)
